93f1201824bc2f98de10c27a457730b25ad1b998
Miguel Angel Vera Beltran <miguel-angel.vera-beltran@outlook.com>
2023-10-10 16:05:28 -0600

    Cambios video 28
    Send a 405 status code and Allow header for invalid request methods

--- src/TaskController.php
+++ src/TaskController.php
@@ -7,7 +7,7 @@ class TaskController
      * @param id identifica el reurso unico (acepta null) con el signo'?'
      * @return void (no retorna nada)
      */
-    public function processRequest(string $method, string $id):void
+    public function processRequest(string $method, ?string $id): void
     {
         if ($id === null) {
             if ($method == "GET") {
@@ -16,9 +16,12 @@ class TaskController
             } elseif ($method == "POST") {
                 //crear task
                 echo "create";
+            } else {
+                //metodos que estan permitidos
+                $this->respondMethodNotAllowed("GET, POST");
             }
         } else {
-            //quiere decir que si existe la task y agregamos un switch para ver que hacer
+            //quiere decir que si existe la task (id) y agregamos un switch para ver que hacer
             switch ($method) {
                     //si es get solo mostramos la task especifica
                 case 'GET':
@@ -32,7 +35,20 @@ class TaskController
                 case 'DELETE':
                     echo "delete $id";
                     break;
+                default:
+                    // si mandas un post con id te dice que solo esta permitido get,patch y delete
+                    $this->respondMethodNotAllowed("GET, PATCH, DELETE");
             }
         }
+    }
+    /**
+     * funcion para manejar los metodos que estan permitidos
+     */
+    private function respondMethodNotAllowed(string $allowed_methods): void
+    {
+        // si envias otro metodo diferente a la url http://localhost/holamundo/api/task debe mostrar error
+        http_response_code(405);
+        //y enviamos al header los metodos que estan permitidos
+        header("Allow: $allowed_methods");
     }
 }
